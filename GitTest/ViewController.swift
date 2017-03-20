a96b2f66a637e883e5d85c04bafca8fd229c644b
Student <Student@zpr-l0007071.local>
2017-03-20 09:09:54 -0400

    Made label and button. Label displays how many times button was pressed

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var count = 0
 
+    @IBOutlet weak var labelText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed(_ sender: Any) { count += 1
+        if count == 1{
+            labelText.text = "Pressed once"
+        }
+    }
 
 }
 
